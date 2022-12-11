acae0c5f7b7d5bab14567d5d2e22e0c764fb47df
Ashwath-learner1 <ashwathkumaruumapathy@gmail.com>
2022-12-11 21:47:42 +0530

    nodes

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-console.log("india");
+import neo4j from 'neo4j-driver';
+var driver=neo4j.driver('bolt://localhost:7687',neo4j.auth.basic("neo4j","ironman3"));
+var session=driver.session();
+const nodes= await session.run("MATCH(x:Person) RETURN x");
+
+
+nodes.records.forEach(function(record){
+    console.log(record._fields[0]);
+});
+
+
+
+  
